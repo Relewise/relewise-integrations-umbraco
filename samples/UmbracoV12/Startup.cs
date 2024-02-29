f860e32a368b2b5350811e37ac1be6bf61bd6e49
Brian Holmgård Kristensen <mail@brianh.dk>
2024-02-29 17:57:22 +0100

    wip

---
+++ samples/UmbracoV12/Startup.cs
@@ -0,0 +1,83 @@
+using Relewise.Integrations.Umbraco;
+
+namespace Relewise.UmbracoV12
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _config;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup" /> class.
+        /// </summary>
+        /// <param name="webHostEnvironment">The web hosting environment.</param>
+        /// <param name="config">The configuration.</param>
+        /// <remarks>
+        /// Only a few services are possible to be injected here https://github.com/dotnet/aspnetcore/issues/9337
+        /// </remarks>
+        public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration config)
+        {
+            _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(webHostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{webHostEnvironment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            _config = builder.Build();
+        }
+
+        /// <summary>
+        /// Configures the services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <remarks>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// </remarks>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddUmbraco(_env, _config)
+                .AddBackOffice()
+                .AddWebsite()
+                .AddComposers()
+                .AddRelewise(options => options
+                    .AddContentType("landingPage", contentType => contentType.AutoMap())
+                    .AddContentType("contentPage", contentType => contentType.AutoMap())
+                    .AddContentType("blogEntry", contentType => contentType.AutoMap()))
+                .Build();
+        }
+
+        /// <summary>
+        /// Configures the application.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">The web hosting environment.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseUmbraco()
+                .WithMiddleware(u =>
+                {
+                    u.UseBackOffice();
+                    u.UseWebsite();
+                    // Enables tracking of all page-views to Relewise
+                    u.TrackContentViews();
+                })
+                .WithEndpoints(u =>
+                {
+                    u.UseInstallerEndpoints();
+                    u.UseBackOfficeEndpoints();
+                    u.UseWebsiteEndpoints();
+                });
+        }
+    }
+}
